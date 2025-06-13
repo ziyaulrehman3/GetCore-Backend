d01cf7a429cf938cb1c128fd2a648c72317424be
Ziya Ul Rehman <farhangolu007@gmail.com>
2025-06-13 23:02:41 +0530

    final

--- node_modules/node-cron/dist/cjs/task-registry.d.ts
+++
@@ -1,9 +0,0 @@
-import { ScheduledTask } from "./tasks/scheduled-task";
-export declare class TaskRegistry {
-    add(task: ScheduledTask): void;
-    get(taskId: string): ScheduledTask | undefined;
-    remove(task: ScheduledTask): void;
-    all(): ScheduledTask[];
-    has(taskId: string): boolean;
-    killAll(): void;
-}
